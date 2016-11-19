1922b9e84dff1106d1a2f60204020314321bee0c
markbarks <marknuttallsmith@gmail.com>
2016-11-19 17:47:55 +0100

    Remove dead code

--- arrowmanager/app.py
+++ arrowmanager/app.py
@@ -45,9 +45,6 @@ def register_extensions(app):
 
 def register_blueprints(app):
     """Register Flask blueprints."""
-    # app.register_blueprint(public.views.blueprint)
-    # app.register_blueprint(user.views.blueprint)
-
     app.register_blueprint(arrows.blueprint)
     return None
 
